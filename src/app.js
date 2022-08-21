9723d0cd25380cadab01859000db34dc6ea14d02
Kateryna <kate.sv.ks@gmail.com>
2022-08-21 16:45:02 +0100

    Added the relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -16,12 +16,14 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 function displayTemperature(response) {
+  //console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconWeatherElement = document.querySelector("#iconWeather");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -29,6 +31,14 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconWeatherElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconWeatherElement.setAttribute(
+    "alt",
+    `response.data.weather[0].description`
+  );
 }
 
 apiKey = "820f87f35ac7df288b102b51b47b967b";
