6f04b3c206c80628559d21c10f337d66f4df73b9
Kateryna <kate.sv.ks@gmail.com>
2022-08-26 14:30:20 +0100

    Added forecast as a loop. Fake data only

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 let celsiusTemperature = null;
+displayForecast();
 
 function formatDate(timestamp) {
   //calculate the date in return with the format "Friday 5:00"
@@ -17,6 +18,34 @@ function formatDate(timestamp) {
   let day = daysWeek[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+          <img
+            src="https://openweathermap.org/img/wn/50d@2x.png"
+            alt=""
+            width="42"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> 18˚ </span>
+            <span class="weather-forecast-temperature-min"> 12˚ </span>
+          </div>
+        
+     </div>
+     `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   //console.log(response);
   let temperatureElement = document.querySelector("#temperature");
