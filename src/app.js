07c898aace352e28bc6d42e468c76c43738315f8
Kateryna <kate.sv.ks@gmail.com>
2022-08-28 14:02:19 +0100

    Removed the Fahrehnheit link

--- src/app.js
+++ src/app.js
@@ -118,17 +118,6 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
 function displayCelsiusTemperature(event) {
   event.preventDefault();
 
@@ -141,9 +130,6 @@ function displayCelsiusTemperature(event) {
 
 let formSearch = document.querySelector("#search-form");
 formSearch.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
